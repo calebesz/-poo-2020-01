0b8b9c21f36d72d6bc26f79022e6080cf809528f
Calebe <calebegohan22@hotmail.com>
2020-12-06 14:19:45 -0300

    Primeiro commit

---
+++ T30/08/src/pkg08/pessoa.java
@@ -0,0 +1,24 @@
+package pkg08;
+
+public class pessoa {
+    private String nome;
+
+    public pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "pessoa{" + "nome=" + nome + "}\n";
+    }
+    
+    
+}
